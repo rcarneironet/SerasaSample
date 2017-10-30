fb4e3f4977ea08cc84c9b8387c5ddb832e519326
rcarneironet <ray.silva@msn.com>
2017-10-30 09:16:41 -0200

    baseline

---
+++ iJetSolutions.SerasaDemo.Forms/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace iJetSolutions.SerasaDemo.Forms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.btnListarProdutos = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Location = new System.Drawing.Point(12, 128);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.Size = new System.Drawing.Size(1195, 256);
+            this.dataGridView.TabIndex = 0;
+            // 
+            // btnListarProdutos
+            // 
+            this.btnListarProdutos.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnListarProdutos.Location = new System.Drawing.Point(12, 12);
+            this.btnListarProdutos.Name = "btnListarProdutos";
+            this.btnListarProdutos.Size = new System.Drawing.Size(116, 64);
+            this.btnListarProdutos.TabIndex = 1;
+            this.btnListarProdutos.Text = "Listar Produtos";
+            this.btnListarProdutos.UseVisualStyleBackColor = true;
+            this.btnListarProdutos.Click += new System.EventHandler(this.btnListarProdutos_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1219, 396);
+            this.Controls.Add(this.btnListarProdutos);
+            this.Controls.Add(this.dataGridView);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.Button btnListarProdutos;
+    }
+}
+
---
+++ iJetSolutions.SerasaDemo.Forms/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Services.Protocols;
+using System.Windows.Forms;
+
+namespace iJetSolutions.SerasaDemo.Forms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnListarProdutos_Click(object sender, EventArgs e)
+        {
+
+            var service = new br.org.spc.treina.consultaWebService();
+            service.Credentials = gerarCredencial("398504", "27102017");
+            var list = service.listarProdutos().ToList();
+        }
+
+        private CredentialCache gerarCredencial(string usuario, string senha)
+        {
+            string url = @"https://treina.spc.org.br/spc/remoting/ws/consulta/consultaWebService?wsdl";
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Ssl3;
+            CredentialCache credentialCache = new CredentialCache();
+            credentialCache.Add(new System.Uri(url), "Basic", new NetworkCredential(usuario, senha));
+            return credentialCache;
+        }
+    }
+}
